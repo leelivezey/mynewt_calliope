04ded6232a1b799f49e19b948f817b7a6b2118ae
Alfred Schilken <alfred@schilken.de>
2017-08-21 23:34:41 +0200

    add ibeacon_conf_handler to configure and store major and minor of ibeacon

--- apps/ibeacon/src/main.c
+++ apps/ibeacon/src/main.c
@@ -1,11 +1,83 @@
 #include "sysinit/sysinit.h"
 #include "os/os.h"
 #include "console/console.h"
+#include "config/config.h"
 #include "host/ble_hs.h"
 
+
+static void update_adv();
+
+static char *ibeacon_conf_get(int argc, char **argv, char *val, int val_len_max);
+
+static int ibeacon_conf_set(int argc, char **argv, char *val);
+
+static int ibeacon_conf_export(void (*export_func)(char *name, char *val), enum conf_export_tgt tgt);
+
+struct conf_handler ibeacon_conf_handler = {
+        .ch_name = "ibeacon",
+        .ch_get = ibeacon_conf_get,
+        .ch_set = ibeacon_conf_set,
+        .ch_export = ibeacon_conf_export,
+        .ch_commit = NULL
+};
+
+static uint16_t major = 2;
+static uint16_t minor = 10;
+static char minor_str[6];
+static char major_str[6];
+
+
 static void
-ble_app_set_addr(void)
+init_config() {
+    int rc = conf_register(&ibeacon_conf_handler);
+    assert(rc == 0);
+}
+
+static char *
+ibeacon_conf_get(int argc, char **argv, char *val, int val_len_max) {
+    if (argc == 1) {
+        if (!strcmp(argv[0], "minor")) {
+            return conf_str_from_value(CONF_INT16, &minor,
+                                       minor_str, sizeof minor_str);
+        } else if (!strcmp(argv[0], "major")) {
+            return conf_str_from_value(CONF_INT16, &major,
+                                       major_str, sizeof major_str);
+        }
+    }
+        return NULL;
+}
+
+static int
+ibeacon_conf_set(int argc, char **argv, char *val) {
+    if (argc == 1) {
+        if (!strcmp(argv[0], "minor")) {
+            int rc = CONF_VALUE_SET(val, CONF_INT16, minor);
+            update_adv();
+            return rc;
+        } else if (!strcmp(argv[0], "major")) {
+            return CONF_VALUE_SET(val, CONF_INT16, major);
+        }
+    }
+    return OS_ENOENT;
+}
+
+static int
+ibeacon_conf_export(void (*export_func)(char *name, char *val),
+               enum conf_export_tgt tgt)
 {
+//    if (tgt == CONF_EXPORT_SHOW) {
+//    }
+// for CONF_EXPORT_SHOW and CONF_EXPORT_PERSIST
+    export_func("ibeacon/minor", conf_str_from_value(CONF_INT16, &minor,
+                                                     minor_str, sizeof minor_str));
+    export_func("ibeacon/major", conf_str_from_value(CONF_INT16, &major,
+                                                     major_str, sizeof major_str));
+    return 0;
+}
+
+
+static void
+ble_app_set_addr(void) {
     ble_addr_t addr;
     int rc;
 
@@ -16,9 +88,9 @@ ble_app_set_addr(void)
     assert(rc == 0);
 }
 
+
 static void
-ble_app_advertise(void)
-{
+ble_app_advertise(void) {
     struct ble_gap_adv_params adv_params;
     uint8_t uuid128[16];
     int rc;
@@ -27,19 +99,25 @@ ble_app_advertise(void)
     memset(uuid128, 0x11, sizeof uuid128);
 
     /* Major version=2; minor version=10. */
-    rc = ble_ibeacon_set_adv_data(uuid128, 2, 10);
+    rc = ble_ibeacon_set_adv_data(uuid128, major, minor);
     assert(rc == 0);
 
     /* Begin advertising. */
-    adv_params = (struct ble_gap_adv_params){ 0 };
+    adv_params = (struct ble_gap_adv_params) {0};
     rc = ble_gap_adv_start(BLE_OWN_ADDR_RANDOM, NULL, BLE_HS_FOREVER,
                            &adv_params, NULL, NULL);
     assert(rc == 0);
 }
 
 static void
-ble_app_on_sync(void)
-{
+update_adv(){
+    ble_gap_adv_stop();
+    ble_app_advertise();
+}
+
+static void
+ble_app_on_sync(void) {
+
     /* Generate a non-resolvable private address. */
     ble_app_set_addr();
 
@@ -48,10 +126,9 @@ ble_app_on_sync(void)
 }
 
 int
-main(int argc, char **argv)
-{
+main(int argc, char **argv) {
     sysinit();
-
+    init_config();
     ble_hs_cfg.sync_cb = ble_app_on_sync;
 
     /* As the last thing, process events from default event queue. */
