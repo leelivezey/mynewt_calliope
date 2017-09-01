e475262d0a15cb1aba6a4694e4d10ecefe222a6c
Alfred Schilken <alfred@schilken.de>
2017-09-01 12:12:41 +0200

    add i2c_toggle_cmd, loop 10000 times an measure time

--- apps/mydrivertest/src/i2c_commands.c
+++ apps/mydrivertest/src/i2c_commands.c
@@ -12,14 +12,16 @@
 #include <hal/hal_i2c.h>
 
 static int i2c_scan_cmd(int argc, char **argv);
-
 static int i2c_get_cmd(int argc, char **argv);
-
 static int i2c_set_cmd(int argc, char **argv);
+static int i2c_toggle_cmd(int argc, char **argv);
 
 #define I2C_MODULE "i2c"
 #define I2C_BUS 0
 static struct hal_i2c_master_data i2c_data;
+
+static uint8_t last_i2c_address;
+static uint8_t last_i2c_reg = 0xff;
 
 /*
  * Initialize param and command help information if SHELL_CMD_HELP
@@ -55,6 +57,11 @@ static const struct shell_cmd_help i2c_scan_help = {
         .usage = NULL,
         .params = NULL,
 };
+static const struct shell_cmd_help i2c_toggle_help = {
+        .summary = "toggle last adr/reg 10000 times",
+        .usage = NULL,
+        .params = NULL,
+};
 #endif
 
 /*
@@ -81,6 +88,13 @@ static const struct shell_cmd i2c_module_commands[] = {
                 .sc_cmd_func = i2c_scan_cmd,
 #if MYNEWT_VAL(SHELL_CMD_HELP)
                 .help = &i2c_scan_help,
+#endif
+        },
+        {
+            .sc_cmd = "toggle",
+            .sc_cmd_func = i2c_toggle_cmd,
+#if MYNEWT_VAL(SHELL_CMD_HELP)
+            .help = &i2c_toggle_help,
 #endif
         },
         {NULL, NULL, NULL},
@@ -156,6 +170,8 @@ i2c_set_cmd(int argc, char **argv) {
         i2c_data.len = 3;
         rc = hal_i2c_master_write(I2C_BUS, &i2c_data, OS_TICKS_PER_SEC, true);
     } else {
+        last_i2c_address = i2c_addr;
+        last_i2c_reg = i2c_reg;
         rc = send_register_and_value((uint8_t)i2c_addr, (uint8_t)i2c_reg, (uint8_t)i2c_val);
     }
     if (rc == 0) {
@@ -223,7 +239,7 @@ i2c_get_cmd(int argc, char **argv) {
     i2c_data.len = i2c_count;
     rc = hal_i2c_master_read(I2C_BUS, &i2c_data, OS_TICKS_PER_SEC, true);
     if (rc == 0) {
-        console_printf("i2c: get %02x %02x\n", i2c_reg, i2c_count);
+        console_printf("i2c: get from reg %02x %02x bytes\n", i2c_reg, i2c_count);
         for(int ix = 0; ix < i2c_count; ix++){
             console_printf("%02x ", command_bytes[ix]);
         }
@@ -234,4 +250,25 @@ i2c_get_cmd(int argc, char **argv) {
     }
 
     return 0;
+}
+
+#define LOOP_COUNT 10000
+
+static int
+i2c_toggle_cmd(int argc, char **argv) {
+    int rc = -1;
+    uint8_t command_bytes[3];
+    command_bytes[0] = last_i2c_reg;
+    command_bytes[1] = 1;
+    i2c_data.address = last_i2c_address;
+    i2c_data.buffer = command_bytes;
+    i2c_data.len = 2;
+    uint32_t start_cputime = os_cputime_get32();
+    for(int nn = 0; nn < LOOP_COUNT; nn++){
+        rc = hal_i2c_master_write(I2C_BUS, &i2c_data, OS_TICKS_PER_SEC, true);
+        command_bytes[1] ^= 0x01;
+    }
+    uint32_t end_cputime = os_cputime_get32();
+    console_printf("%d times: start: %d, end: %d, delta: %d\n", LOOP_COUNT, (int)start_cputime, (int)end_cputime, (int)(end_cputime-start_cputime));
+    return rc;
 }
