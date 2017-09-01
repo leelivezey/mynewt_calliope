59a450e264672ae1efb7eb71b6257cd8e9d1ebdb
Alfred Schilken <alfred@schilken.de>
2017-09-01 21:42:46 +0200

    add peer.c to ancs

--- apps/ble_ancs/src/bleprph.h
+++ apps/ble_ancs/src/bleprph.h
@@ -51,6 +51,12 @@ int gatt_svr_init(void);
 
 /** Misc. */
 void print_bytes(const uint8_t *bytes, int len);
+void print_mbuf(const struct os_mbuf *om);
+char *addr_str(const void *addr);
+void print_uuid(const ble_uuid_t *uuid);
+void print_conn_desc(const struct ble_gap_conn_desc *desc);
+void print_adv_fields(const struct ble_hs_adv_fields *fields);
+
 void print_addr(const void *addr);
 
 #ifdef __cplusplus
--- apps/ble_ancs/src/main.c
+++ apps/ble_ancs/src/main.c
@@ -21,6 +21,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <host/ble_uuid.h>
 #include "sysinit/sysinit.h"
 #include "bsp/bsp.h"
 #include "os/os.h"
@@ -38,10 +39,29 @@
 /* Application-specified header. */
 #include "bleprph.h"
 
+#include "peer.h"
+
 
 static const ble_uuid128_t ancs_base_uuid128 =
         BLE_UUID128_INIT(0xd0, 0x00, 0x2d, 0x12, 0x1e, 0x4b, 0x0f, 0xa4,
-                0x99, 0x4e, 0xce, 0xb5, 0x31, 0xf4, 0x05, 0x79);
+                         0x99, 0x4e, 0xce, 0xb5, 0x31, 0xf4, 0x05, 0x79);
+
+static const ble_uuid128_t notification_source_uuid =
+        BLE_UUID128_INIT(0xbd, 0x1d, 0xa2, 0x99, 0xe6, 0x25, 0x58, 0x8c,
+                         0xd9, 0x42, 0x01, 0x63, 0x0d, 0x12, 0xbf, 0x9f);
+
+static const ble_uuid128_t control_point_uuid =
+        BLE_UUID128_INIT(0xd9, 0xd9, 0xaa, 0xfd, 0xbd, 0x9b, 0x21, 0x98,
+                         0xa8, 0x49, 0xe1, 0x45, 0xf3, 0xd8, 0xd1, 0x69);
+
+static const ble_uuid128_t data_source_uuid128 =
+        BLE_UUID128_INIT(0xfb, 0x7b, 0x7c, 0xce, 0x6a, 0xb3, 0x44, 0xbe,
+                         0xb5, 0x4b, 0xd6, 0x24, 0xe9, 0xc6, 0xea, 0x22);
+
+//const UUID ancs_uuid("7905F431B5CE4E99A40F4B1E122D00D0");
+//const UUID notification_source_uuid("9F BF 12 0D 63 01 42 D9 8C 58 25 E6 99 A2 1D BD");
+//const UUID       control_point_uuid("69 D1 D8 F3 45 E1 49 A8 98 21 9B BD FD AA D9 D9");
+//const UUID         data_source_uuid("22 EA C6 E9 24 D6 4B B5 BE 44 B3 6A CE 7C 7B FB");
 
 /** Log data. */
 struct log bleprph_log;
@@ -52,8 +72,7 @@ static int bleprph_gap_event(struct ble_gap_event *event, void *arg);
  * Logs information about a connection to the console.
  */
 static void
-bleprph_print_conn_desc(struct ble_gap_conn_desc *desc)
-{
+bleprph_print_conn_desc(struct ble_gap_conn_desc *desc) {
     BLEPRPH_LOG(INFO, "handle=%d our_ota_addr_type=%d our_ota_addr=",
                 desc->conn_handle, desc->our_ota_addr.type);
     print_addr(desc->our_ota_addr.val);
@@ -67,7 +86,7 @@ bleprph_print_conn_desc(struct ble_gap_conn_desc *desc)
                 desc->peer_id_addr.type);
     print_addr(desc->peer_id_addr.val);
     BLEPRPH_LOG(INFO, " conn_itvl=%d conn_latency=%d supervision_timeout=%d "
-                "encrypted=%d authenticated=%d bonded=%d\n",
+            "encrypted=%d authenticated=%d bonded=%d\n",
                 desc->conn_itvl, desc->conn_latency,
                 desc->supervision_timeout,
                 desc->sec_state.encrypted,
@@ -81,8 +100,7 @@ bleprph_print_conn_desc(struct ble_gap_conn_desc *desc)
  *     o Undirected connectable mode.
  */
 static void
-bleprph_advertise(void)
-{
+bleprph_advertise(void) {
     struct ble_gap_adv_params adv_params;
     struct ble_hs_adv_fields fields;
     int rc;
@@ -112,7 +130,7 @@ bleprph_advertise(void)
     fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
 
     fields.uuids128 = BLE_UUID128(&ancs_base_uuid128.u);
-    print_bytes((const uint8_t *)fields.uuids128, 16);
+    print_bytes((const uint8_t *) fields.uuids128, 16);
     fields.num_uuids128 = 1;
     fields.uuids128_is_complete = 1;
 
@@ -122,8 +140,8 @@ bleprph_advertise(void)
     }
 
     memset(&fields, 0, sizeof fields);
-    fields.name = (uint8_t *)ble_svc_gap_device_name();
-    fields.name_len = strlen((char *)fields.name);
+    fields.name = (uint8_t *) ble_svc_gap_device_name();
+    fields.name_len = strlen((char *) fields.name);
     fields.name_is_complete = 1;
 
     rc = ble_gap_adv_rsp_set_fields(&fields);
@@ -143,6 +161,82 @@ bleprph_advertise(void)
     }
 }
 
+#if 0
+static void start_discovery() {
+/**
+ * Performs the Limited or General Discovery Procedures.
+ *
+ * @param own_addr_type         The type of address the stack should use for
+ *                                  itself when sending scan requests.  Valid
+ *                                  values are:
+ *                                      o BLE_ADDR_TYPE_PUBLIC
+ *                                      o BLE_ADDR_TYPE_RANDOM
+ *                                      o BLE_ADDR_TYPE_RPA_PUB_DEFAULT
+ *                                      o BLE_ADDR_TYPE_RPA_RND_DEFAULT
+ *                                  This parameter is ignored unless active
+ *                                  scanning is being used.
+ * @param duration_ms           The duration of the discovery procedure.
+ *                                  On expiration, the procedure ends and a
+ *                                  BLE_GAP_EVENT_DISC_COMPLETE event is
+ *                                  reported.  Units are milliseconds.  Specify
+ *                                  BLE_HS_FOREVER for no expiration.
+ * @param disc_params           Additional arguments specifying the particulars
+ *                                  of the discovery procedure.
+ * @param cb                    The callback to associate with this discovery
+ *                                  procedure.  Advertising reports and
+ *                                  discovery termination events are reported
+ *                                  through this callback.
+ * @param cb_arg                The optional argument to pass to the callback
+ *                                  function.
+ *
+ * @return                      0 on success; nonzero on failure.
+ */
+    struct ble_gap_disc_params disc_params = {0};
+    disc_params.filter_duplicates = 1;
+
+    /**
+     * Perform a passive scan.  I.e., don't send follow-up scan requests to
+     * each advertiser.
+     */
+    disc_params.passive = 1;
+    disc_params.itvl = 0;
+    disc_params.window = 0;
+    disc_params.filter_policy = 0;
+    disc_params.limited = 0;
+
+    int rc = ble_gap_disc(BLE_OWN_ADDR_PUBLIC, 20000, &disc_params, bleprph_gap_event, NULL);
+    BLEPRPH_LOG(INFO, "ble_gap_disc rc: %d\n", rc);
+}
+#endif
+
+/**
+ * Called when service discovery of the specified peer has completed.
+ */
+static void
+blecent_on_disc_complete(const struct peer *peer, int status, void *arg)
+{
+
+    if (status != 0) {
+        /* Service discovery failed.  Terminate the connection. */
+        BLEPRPH_LOG(ERROR, "Error: Service discovery failed; status=%d "
+                "conn_handle=%d\n", status, peer->conn_handle);
+        ble_gap_terminate(peer->conn_handle, BLE_ERR_REM_USER_CONN_TERM);
+        return;
+    }
+
+    /* Service discovery has completed successfully.  Now we have a complete
+     * list of services, characteristics, and descriptors that the peer
+     * supports.
+     */
+    BLEPRPH_LOG(ERROR, "Service discovery complete; status=%d "
+            "conn_handle=%d\n", status, peer->conn_handle);
+
+    /* Now perform three concurrent GATT procedures against the peer: read,
+     * write, and subscribe to notifications.
+     */
+//    blecent_read_write_subscribe(peer);
+}
+
 /**
  * The nimble host executes this callback when a GAP event occurs.  The
  * application associates a GAP event callback with each connection that forms.
@@ -159,110 +253,157 @@ bleprph_advertise(void)
  *                                  particular GAP event being signalled.
  */
 static int
-bleprph_gap_event(struct ble_gap_event *event, void *arg)
-{
+bleprph_gap_event(struct ble_gap_event *event, void *arg) {
     struct ble_gap_conn_desc desc;
+    struct ble_hs_adv_fields fields;
     int rc;
+    char s[BLE_HS_ADV_MAX_SZ];
 
     BLEPRPH_LOG(INFO, "bleprph_gap_event %d\n", event->type);
 
     switch (event->type) {
-    case BLE_GAP_EVENT_CONNECT:
-        /* A new connection was established or a connection attempt failed. */
-        BLEPRPH_LOG(INFO, "connection %s; status=%d ",
-                       event->connect.status == 0 ? "established" : "failed",
-                       event->connect.status);
-        if (event->connect.status == 0) {
+        case BLE_GAP_EVENT_CONNECT:
+            /* A new connection was established or a connection attempt failed. */
+            BLEPRPH_LOG(INFO, "connection %s; status=%d ",
+                        event->connect.status == 0 ? "established" : "failed",
+                        event->connect.status);
+            if (event->connect.status == 0) {
+                rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
+                assert(rc == 0);
+                bleprph_print_conn_desc(&desc);
+            }
+            BLEPRPH_LOG(INFO, "\n");
+
+            if (event->connect.status != 0) {
+                /* Connection failed; resume advertising. */
+                bleprph_advertise();
+            } else {
+                /* Remember peer. */
+                rc = peer_add(event->connect.conn_handle);
+                if (rc != 0) {
+                    BLEPRPH_LOG(ERROR, "Failed to add peer; rc=%d\n", rc);
+                    return 0;
+                }
+
+                /* Perform service discovery. */
+                rc = peer_disc_all(event->connect.conn_handle,
+                                   blecent_on_disc_complete, NULL);
+                if (rc != 0) {
+                    BLEPRPH_LOG(ERROR, "Failed to discover services; rc=%d\n", rc);
+                    return 0;
+                }
+            }
+            return 0;
+
+        case BLE_GAP_EVENT_DISCONNECT:
+            BLEPRPH_LOG(INFO, "disconnect; reason=%d ", event->disconnect.reason);
+            bleprph_print_conn_desc(&event->disconnect.conn);
+            BLEPRPH_LOG(INFO, "\n");
+
+            /* Connection terminated; resume advertising. */
+            bleprph_advertise();
+            return 0;
+
+        case BLE_GAP_EVENT_CONN_UPDATE:
+            /* The central has updated the connection parameters. */
+            BLEPRPH_LOG(INFO, "connection updated; status=%d ",
+                        event->conn_update.status);
             rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
             assert(rc == 0);
             bleprph_print_conn_desc(&desc);
-        }
-        BLEPRPH_LOG(INFO, "\n");
+            BLEPRPH_LOG(INFO, "\n");
+            return 0;
 
-        if (event->connect.status != 0) {
-            /* Connection failed; resume advertising. */
-            bleprph_advertise();
-        }
-        return 0;
+        case BLE_GAP_EVENT_ENC_CHANGE:
+            /* Encryption has been enabled or disabled for this connection. */
+            BLEPRPH_LOG(INFO, "encryption change event; status=%d ",
+                        event->enc_change.status);
+            rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
+            assert(rc == 0);
+            bleprph_print_conn_desc(&desc);
+            BLEPRPH_LOG(INFO, "\n");
+            return 0;
 
-    case BLE_GAP_EVENT_DISCONNECT:
-        BLEPRPH_LOG(INFO, "disconnect; reason=%d ", event->disconnect.reason);
-        bleprph_print_conn_desc(&event->disconnect.conn);
-        BLEPRPH_LOG(INFO, "\n");
+        case BLE_GAP_EVENT_SUBSCRIBE:
+            BLEPRPH_LOG(INFO, "subscribe event; conn_handle=%d attr_handle=%d "
+                    "reason=%d prevn=%d curn=%d previ=%d curi=%d\n",
+                        event->subscribe.conn_handle,
+                        event->subscribe.attr_handle,
+                        event->subscribe.reason,
+                        event->subscribe.prev_notify,
+                        event->subscribe.cur_notify,
+                        event->subscribe.prev_indicate,
+                        event->subscribe.cur_indicate);
+            return 0;
 
-        /* Connection terminated; resume advertising. */
-        bleprph_advertise();
-        return 0;
+        case BLE_GAP_EVENT_MTU:
+            BLEPRPH_LOG(INFO, "mtu update event; conn_handle=%d cid=%d mtu=%d\n",
+                        event->mtu.conn_handle,
+                        event->mtu.channel_id,
+                        event->mtu.value);
+            return 01;
 
-    case BLE_GAP_EVENT_CONN_UPDATE:
-        /* The central has updated the connection parameters. */
-        BLEPRPH_LOG(INFO, "connection updated; status=%d ",
-                    event->conn_update.status);
-        rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
-        assert(rc == 0);
-        bleprph_print_conn_desc(&desc);
-        BLEPRPH_LOG(INFO, "\n");
-        return 0;
+        case BLE_GAP_EVENT_REPEAT_PAIRING:
+            /* We already have a bond with the peer, but it is attempting to
+             * establish a new secure link.  This app sacrifices security for
+             * convenience: just throw away the old bond and accept the new link.
+             */
 
-    case BLE_GAP_EVENT_ENC_CHANGE:
-        /* Encryption has been enabled or disabled for this connection. */
-        BLEPRPH_LOG(INFO, "encryption change event; status=%d ",
-                    event->enc_change.status);
-        rc = ble_gap_conn_find(event->connect.conn_handle, &desc);
-        assert(rc == 0);
-        bleprph_print_conn_desc(&desc);
-        BLEPRPH_LOG(INFO, "\n");
-        return 0;
+            /* Delete the old bond. */
+            rc = ble_gap_conn_find(event->repeat_pairing.conn_handle, &desc);
+            assert(rc == 0);
+            ble_store_util_delete_peer(&desc.peer_id_addr);
 
-    case BLE_GAP_EVENT_SUBSCRIBE:
-        BLEPRPH_LOG(INFO, "subscribe event; conn_handle=%d attr_handle=%d "
-                          "reason=%d prevn=%d curn=%d previ=%d curi=%d\n",
-                    event->subscribe.conn_handle,
-                    event->subscribe.attr_handle,
-                    event->subscribe.reason,
-                    event->subscribe.prev_notify,
-                    event->subscribe.cur_notify,
-                    event->subscribe.prev_indicate,
-                    event->subscribe.cur_indicate);
-        return 0;
+            /* Return BLE_GAP_REPEAT_PAIRING_RETRY to indicate that the host should
+             * continue with the pairing operation.
+             */
+            return BLE_GAP_REPEAT_PAIRING_RETRY;
 
-    case BLE_GAP_EVENT_MTU:
-        BLEPRPH_LOG(INFO, "mtu update event; conn_handle=%d cid=%d mtu=%d\n",
-                    event->mtu.conn_handle,
-                    event->mtu.channel_id,
-                    event->mtu.value);
-        return 0;
+        case BLE_GAP_EVENT_DISC:
 
-    case BLE_GAP_EVENT_REPEAT_PAIRING:
-        /* We already have a bond with the peer, but it is attempting to
-         * establish a new secure link.  This app sacrifices security for
-         * convenience: just throw away the old bond and accept the new link.
-         */
+            rc = ble_hs_adv_parse_fields(&fields, event->disc.data,
+                                         event->disc.length_data);
+            BLEPRPH_LOG(INFO, "BLE_GAP_EVENT_DISC rc=%d, flags=0x%02x\n", rc, fields.flags);
+            if (rc != 0) {
+                return 0;
+            }
 
-        /* Delete the old bond. */
-        rc = ble_gap_conn_find(event->repeat_pairing.conn_handle, &desc);
-        assert(rc == 0);
-        ble_store_util_delete_peer(&desc.peer_id_addr);
+            if (fields.svc_data_uuid128 != NULL) {
+                BLEPRPH_LOG(INFO, "    svc_data_uuid128=");
+                print_bytes(fields.svc_data_uuid128, fields.svc_data_uuid128_len);
+                BLEPRPH_LOG(DEBUG, "\n");
+            }
+            if (fields.name != NULL) {
+                assert(fields.name_len < sizeof s - 1);
+                memcpy(s, fields.name, fields.name_len);
+                s[fields.name_len] = '\0';
+                BLEPRPH_LOG(INFO, "    name(%scomplete)=%s\n",
+                            fields.name_is_complete ? "" : "in", s);
+            }
+            if (fields.uuids128 != NULL) {
+                BLEPRPH_LOG(INFO, "    uuids128(%scomplete)=",
+                            fields.uuids128_is_complete ? "" : "in");
+                for (int i = 0; i < fields.num_uuids128; i++) {
+                    print_uuid(&fields.uuids128[i].u);
+                    BLEPRPH_LOG(INFO, " ");
+                }
+                BLEPRPH_LOG(DEBUG, "\n");
+            }
 
-        /* Return BLE_GAP_REPEAT_PAIRING_RETRY to indicate that the host should
-         * continue with the pairing operation.
-         */
-        return BLE_GAP_REPEAT_PAIRING_RETRY;
     }
 
     return 0;
 }
 
 static void
-bleprph_on_reset(int reason)
-{
+bleprph_on_reset(int reason) {
     BLEPRPH_LOG(ERROR, "Resetting state; reason=%d\n", reason);
 }
 
 static void
-bleprph_on_sync(void)
-{
+bleprph_on_sync(void) {
     /* Begin advertising. */
+//    start_discovery();
     bleprph_advertise();
 }
 
@@ -275,12 +416,11 @@ bleprph_on_sync(void)
  * @return int NOTE: this function should never return!
  */
 int
-main(void)
-{
+main(void) {
     int rc;
 
     /* Set initial BLE device address. */
-    memcpy(g_dev_addr, (uint8_t[6]){0x0b, 0x0b, 0x0b, 0x0b, 0x0c, 0x0c}, 6);
+    memcpy(g_dev_addr, (uint8_t[6]) {0x0b, 0x0b, 0x0b, 0x0b, 0x0c, 0x0c}, 6);
 
     /* Initialize OS */
     sysinit();
@@ -296,6 +436,10 @@ main(void)
     ble_hs_cfg.sync_cb = bleprph_on_sync;
     ble_hs_cfg.gatts_register_cb = gatt_svr_register_cb;
     ble_hs_cfg.store_status_cb = ble_store_util_status_rr;
+
+    /* Initialize data structures to track connected peers. */
+    rc = peer_init(MYNEWT_VAL(BLE_MAX_CONNECTIONS), 5, 3, 1);
+    assert(rc == 0);
 
     rc = gatt_svr_init();
     assert(rc == 0);
