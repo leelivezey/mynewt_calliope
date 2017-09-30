69b141ac8edfadf2f2ff300c97dd4b6ee2bece1a
alfred schilken <alfred@schilken.de>
2017-09-30 13:03:28 +0200

    add interrupt handler

--- apps/heartbeat/src/main.c
+++ apps/heartbeat/src/main.c
@@ -27,6 +27,7 @@
 #include "hal/hal_gpio.h"
 #include <microbit_matrix/microbit_matrix.h>
 #include <buttons/button_polling.h>
+#include <console/console.h>
 
 #ifdef ARCH_sim
 #include "mcu/mcu_sim.h"
@@ -40,6 +41,11 @@ static struct os_callout blinky_callout;
 
 static char ch = ' ';
 static bool running = false;
+static int new_irq_millisec = 3;
+static int last_irq_millisec;
+
+#define SAMPLES_DELAY    3
+#define GROVE_A1_P1      26
 
 /**
   * This function will be called when the gpio_irq_handle_event is pulled
@@ -59,12 +65,29 @@ button_callback(struct os_event *ev)
 
 // Event callback function for timer events. It toggles the led pin.
 
+static char buffer[2];
+
 static void
 timer_ev_cb(struct os_event *ev) {
     assert(ev != NULL);
     ++g_task1_loops;
-    hal_gpio_toggle(g_led_pin);
-    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC);
+    int32_t ticks = 1;
+    if (running) {
+        ticks = SAMPLES_DELAY;
+//        buffer[0] = hal_gpio_read(GROVE_A1_P1) ? '1' : '0';
+//        console_write(buffer, sizeof(buffer));
+        if(new_irq_millisec != last_irq_millisec) {
+            buffer[0] = '1';
+ //           console_printf("%d\n", new_irq_millisec);
+            last_irq_millisec = new_irq_millisec;
+        } else {
+            buffer[0] = '0';
+        }
+        console_write(buffer, sizeof(buffer));
+    } else {
+        hal_gpio_toggle(g_led_pin);
+    }
+    os_callout_reset(&blinky_callout, ticks);
 }
 
 static void init_timer(void) {
@@ -72,6 +95,12 @@ static void init_timer(void) {
     os_callout_init(&blinky_callout, os_eventq_dflt_get(), timer_ev_cb, NULL);
     os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC);
 }
+
+
+void interruptHandler(void *unused) {
+    new_irq_millisec = os_get_uptime_usec() / 1000;
+}
+
 
 /**
  * main
@@ -92,6 +121,13 @@ main(int argc, char **argv)
     g_led_pin = LED_COL2;
     hal_gpio_init_out(LED_ROW1, 1);
     hal_gpio_init_out(g_led_pin, 0);
+    hal_gpio_init_in(GROVE_A1_P1, HAL_GPIO_PULL_DOWN);
+
+    rc = hal_gpio_irq_init(GROVE_A1_P1, interruptHandler, NULL,
+                           HAL_GPIO_TRIG_RISING, HAL_GPIO_PULL_DOWN);
+    hal_gpio_irq_enable(GROVE_A1_P1);
+    assert(rc == 0);
+    buffer[1] = '\n';
     init_timer();
 
     microbit_set_button_cb(button_callback);
